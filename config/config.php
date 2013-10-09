c8258e48319143365e09d6310f8109c22bcd296c
evangelion62 <sebastien.ronger.evangelion@gmail.com>
2013-10-09 12:40:54 +0200

    refonte
    config.php
    index.php

---
+++ config/config.php
@@ -0,0 +1,42 @@
+<?php
+//admin user
+$admin 		= 	array					(
+		'id'	=>	'admin'				,
+		'pass'=>	'AdminLm/2013');
+
+//bdd config
+$bddConfig 	= 	array				(
+		'host'	=>	'localhost'	,
+		'dbname'=>	'siteclm'		,
+		'user'	=>	'root'			,
+		'pwd'		=>	''					);
+
+//connexion à la bdd
+try
+{
+	$bdd = new PDO('mysql:host='.$bddConfig['host'].';dbname='.$bddConfig['dbname'], $bddConfig['user'], $bddConfig['pwd']);
+}
+catch (Exception $e)
+{
+	die('Erreur : ' . $e->getMessage());
+}
+
+//supression des données de connexion
+$bddConfig = array();
+
+//définition des varriable controler et action
+if (!empty($_GET['controler'])) {
+	$_GET['controler']=stripslashes($_GET['controler']);
+	$_GET['controler']=htmlspecialchars($_GET['controler']);
+	$controler='controlers/'.$_GET['controler'].'.php';
+}else{
+	$controler='controlers/index.php';
+}
+
+if (!empty($_GET['action'])){
+	$_GET['action']=stripslashes($_GET['action']);
+	$_GET['action']=htmlspecialchars($_GET['action']);
+	$action=$_GET['action'];
+}else{
+	$action='index';
+}
--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-<?php header("Location:home/"); ?>
+<?php
+//inclusion du fichier de configuration
+require_once 'config/config.php';
+
+//inclusion du bon controler
+if (is_file($controler)){
+	require_once $controler;
+}else{
+	echo $controler;
+}
