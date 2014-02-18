52dcecfbacc3646c915918716678f3a56915df91
docdoxey <vbvincentboulanger@gmail.com>
2014-02-18 16:12:31 +0100

    controller
    front controller

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <?php
-//inclusion du fichier de configuration
-require_once 'config/config.php';
+	require_once ('config/config.php');
 
-//inclusion du bon controler
-if (is_file($controler)){
-	require_once $controler;
-}else{
-	echo $controler;
-}
+	if (is_file($controler)){
+		require_once $controler;
+	}else{
+		require_once ('controlers/index');
+	}
