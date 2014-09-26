4c1a90fc3c36bd988d4fba846fe9f842f58550f6
sebastien <sebastien.ronger.evangelion@gmail.com>
2014-09-26 22:52:20 +0200

    rectification d'indentation

--- template/navbar.php
+++ template/navbar.php
@@ -11,29 +11,28 @@
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-		<a class="navbar-brand" href="http://www.collegelouisemichellille.fr">Collège Louise Michel Lille</a>
-		</div>
+					<a class="navbar-brand" href="http://www.collegelouisemichellille.fr">Collège Louise Michel Lille</a>
+				</div>
 				<div class="navbar-collapse collapse">
-				<ul class="nav navbar-nav">
-				<li class="active"><a href="#">Home</a></li>
-				<li><a href="#about">About</a></li>
-				<li><a href="#contact">Contact</a></li>
-				<li class="dropdown">
-				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
-				<ul class="dropdown-menu" role="menu">
-				<li><a href="#">Action</a></li>
-				<li><a href="#">Another action</a></li>
-				<li><a href="#">Something else here</a></li>
-						<li class="divider"></li>
-						<li class="dropdown-header">Nav header</li>
-						<li><a href="#">Separated link</a></li>
-						<li><a href="#">One more separated link</a></li>
-						</ul>
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="#">Home</a></li>
+						<li><a href="#about">About</a></li>
+						<li><a href="#contact">Contact</a></li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+							<ul class="dropdown-menu" role="menu">
+								<li><a href="#">Action</a></li>
+								<li><a href="#">Another action</a></li>
+								<li><a href="#">Something else here</a></li>
+								<li class="divider"></li>
+								<li class="dropdown-header">Nav header</li>
+								<li><a href="#">Separated link</a></li>
+								<li><a href="#">One more separated link</a></li>
+							</ul>
 						</li>
-						</ul>
-						</div>
-								</div>
-								</div>
-
+					</ul>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
