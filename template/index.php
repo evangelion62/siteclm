5f75ba0c4bf000a2a0aa9991e39d69d3f20bf201
docdoxey <vbvincentboulanger@gmail.com>
2014-10-03 15:25:13 +0200

    template : index =>crea + modif
    ajout des vignettes et des textes des actualités + ajout d'un bouton
    "plus d'actualités" en bas de page

--- template/index.php
+++ template/index.php
@@ -5,6 +5,7 @@
     <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
     <li data-target="#carousel-example-generic" data-slide-to="1"></li>
     <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="3"></li>
   </ol>
 
   <!-- Wrapper for slides -->
@@ -38,7 +39,6 @@
       </div>
     </div>
   </div>
-
   <!-- Controls -->
   <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
     <span class="glyphicon glyphicon-chevron-left"></span>
@@ -47,6 +47,46 @@
     <span class="glyphicon glyphicon-chevron-right"></span>
   </a>
 </div>
+<br>
+<div class="row featurette">
+    <div class="col-md-5">
+      <img class="featurette-image img-responsive" src="web/img/pink.gif" alt="Generic placeholder image">
+    </div>
+    <div class="col-md-7">
+      <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
+      <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+    </div>
+</div>
+<hr>
+<div class="row featurette">
+    <div class="col-md-7">
+    	<h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
+      	<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+    </div>
+    <div class="col-md-5">
+    	<img class="featurette-image img-responsive" src="web/img/orange.gif" alt="Generic placeholder image">
+    </div>
+</div>
+<div class="row featurette">
+    <div class="col-md-5">
+      <img class="featurette-image img-responsive" src="web/img/pink.gif" alt="Generic placeholder image">
+    </div>
+    <div class="col-md-7">
+      <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
+      <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+    </div>
+</div>
+<hr>
+<div class="row featurette">
+    <div class="col-md-7">
+    	<h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
+      	<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+    </div>
+    <div class="col-md-5">
+    	<img class="featurette-image img-responsive" src="web/img/orange.gif" alt="Generic placeholder image">
+    </div>
+</div>
+<div><button type="button" class="btn btn-lg btn-primary" disabled="disabled">Plus d'actualité</button></div>
 <?php
 $contents = ob_get_clean();
 if (is_file('template/layout.php')){
