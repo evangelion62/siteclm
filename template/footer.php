5ec14a21b98c1806fde3c96b0c9d3079b302d5bf
docdoxey <vbvincentboulanger@gmail.com>
2014-10-03 13:38:20 +0200

    template : footer=>créa :
    Contenu du footer + ajustement

--- template/footer.php
+++ template/footer.php
@@ -2,14 +2,49 @@
 	<hr>
 	<footer class="footer" >
         <div class="row">
-        	<div class="col-md-4" >
-				Hac ita persuasione reducti intra moenia bellatores obseratis undique portarum aditibus, propugnaculis insistebant et pinnis, congesta undique saxa telaque habentes in promptu, ut si quis se proripuisset interius, multitudine missilium sterneretur et lapidum.
+        	<div class="col-md-2" >
+        		<h4>Adresses utiles</h4>
+        		<ol class="list-unstyled">
+					<li>Violence</li>
+					<li>Inspection académique</li>
+					<li>Rectorat</li>					
+				</ol>
 			</div>
-        	<div class="col-md-4">
-        		Hac ita persuasione reducti intra moenia bellatores obseratis undique portarum aditibus, propugnaculis insistebant et pinnis, congesta undique saxa telaque habentes in promptu, ut si quis se proripuisset interius, multitudine missilium sterneretur et lapidum.
+        	<div class="col-md-2">
+        		<h4>Prévention Médiation</h4>
+        		<ol class="list-unstyled">
+					<li>Citeo</li>
+					<li>Itinéraire</li>
+				</ol>
 			</div>
-        	<div class="col-md-4">
-        		Hac ita persuasione reducti intra moenia bellatores obseratis undique portarum aditibus, propugnaculis insistebant et pinnis, congesta undique saxa telaque habentes in promptu, ut si quis se proripuisset interius, multitudine missilium sterneretur et lapidum.
+        	<div class="col-md-3">
+        		<h4>Informations établissement</h4>
+        		<ol class="list-unstyled">
+        			<li>Le projet étab</li>
+					<li>RI</li>
+					<li>Liste du personnel</li>
+					<li>Professeurs principaux</li>
+					<li>Liste des fournitures scolaires</li>
+        		</ol>
+			</div>
+			<div class="col-md-2">
+        		<h4>Informations légales</h4>
+        		<ol class="list-unstyled">
+        			<li>Droit à l'image</li>
+					<li>RI</li>
+        		</ol>
+			</div>
+			<div class="col-md-2">
+				<h4>Les + du collège</h4>
+				<ol class="list-unstyled">
+        			<li>LV</li>
+					<li>DP3</li>
+					<li>Alternance</li>
+					<li>Projet</li>
+					<li>Etude</li>
+					<li>Activités</li>
+					<li>ZUP de Co</li>
+        		</ol>
 			</div>
         </div>
     </footer>
