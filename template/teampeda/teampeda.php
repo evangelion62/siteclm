92835c7047d8b994eda00f1266ba312e907a90e0
docdoxey <vbvincentboulanger@gmail.com>
2014-10-06 14:13:10 +0200

    template : création d'une page teampéda
    ajout d'un thumbails

---
+++ template/teampeda/teampeda.php
@@ -0,0 +1,23 @@
+<?php
+
+/*Accessible à ?controler=user&action=teamPeda*/
+
+ob_start();?>
+	<div class="row">
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img data-src="holder.js/300x300" alt="...">
+      <div class="caption">
+        <h3>Equipe pédagogique</h3>
+        <p>...</p>
+        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+      </div>
+     </div>
+    </div>
+ </div>
+<?php
+$contents = ob_get_clean();
+if (is_file('template/layout/layout.php')){
+	require_once 'template/layout/layout.php';
+}
+?>
