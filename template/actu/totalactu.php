d7559daa00868d2d4d6bf00fb5b0a71e5d0f392b
docdoxey <vbvincentboulanger@gmail.com>
2014-10-06 10:43:35 +0200

    template : totalactu => modif
    ajout d'une liste de d'actualités avec image et texte. tiré de : "Class
    media"

--- template/actu/totalactu.php
+++ template/actu/totalactu.php
@@ -1,13 +1,157 @@
 <?php ob_start();?>
 	<div class="media">
 	  <a class="pull-left" href="#">
-	    <img class="media-object" src="web/img/orange.gif" alt="...">
+	    <img class="media-object" src="web/img/vert-64x64.gif" alt="...">
 	  </a>
 	  <div class="media-body">
 	    <h4 class="media-heading">Media heading</h4>
 	    ...
 	  </div>
 	</div>
+	<div class="media">
+  <a class="pull-left" href="#">
+    <img class="media-object" src="web/img/vert-64x64.gif" alt="...">
+  </a>
+  <div class="media-body">
+    <h4 class="media-heading">Media heading</h4>
+    ...
+  </div>
+  <div class="media">
+	  <a class="pull-left" href="#">
+	    <img class="media-object" src="web/img/vert-64x64.gif" alt="...">
+	  </a>
+	  <div class="media-body">
+	    <h4 class="media-heading">Media heading</h4>
+	    ...
+	  </div>
+	</div>
+	<div class="media">
+  <a class="pull-left" href="#">
+    <img class="media-object" src="web/img/vert-64x64.gif" alt="...">
+  </a>
+  <div class="media-body">
+    <h4 class="media-heading">Media heading</h4>
+    ...
+  </div>
+  <div class="media">
+	  <a class="pull-left" href="#">
+	    <img class="media-object" src="web/img/vert-64x64.gif" alt="...">
+	  </a>
+	  <div class="media-body">
+	    <h4 class="media-heading">Media heading</h4>
+	    ...
+	  </div>
+	</div>
+	<div class="media">
+  <a class="pull-left" href="#">
+    <img class="media-object" src="web/img/vert-64x64.gif" alt="...">
+  </a>
+  <div class="media-body">
+    <h4 class="media-heading">Media heading</h4>
+    ...
+  </div>
+  <div class="media">
+	  <a class="pull-left" href="#">
+	    <img class="media-object" src="web/img/vert-64x64.gif" alt="...">
+	  </a>
+	  <div class="media-body">
+	    <h4 class="media-heading">Media heading</h4>
+	    ...
+	  </div>
+	</div>
+	<div class="media">
+  <a class="pull-left" href="#">
+    <img class="media-object" src="web/img/vert-64x64.gif" alt="...">
+  </a>
+  <div class="media-body">
+    <h4 class="media-heading">Media heading</h4>
+    ...
+  </div>
+  <div class="media">
+	  <a class="pull-left" href="#">
+	    <img class="media-object" src="web/img/vert-64x64.gif" alt="...">
+	  </a>
+	  <div class="media-body">
+	    <h4 class="media-heading">Media heading</h4>
+	    ...
+	  </div>
+	</div>
+	<div class="media">
+  <a class="pull-left" href="#">
+    <img class="media-object" src="web/img/vert-64x64.gif" alt="...">
+  </a>
+  <div class="media-body">
+    <h4 class="media-heading">Media heading</h4>
+    ...
+  </div>
+  <div class="media">
+	  <a class="pull-left" href="#">
+	    <img class="media-object" src="web/img/vert-64x64.gif" alt="...">
+	  </a>
+	  <div class="media-body">
+	    <h4 class="media-heading">Media heading</h4>
+	    ...
+	  </div>
+	</div>
+	<div class="media">
+  <a class="pull-left" href="#">
+    <img class="media-object" src="web/img/vert-64x64.gif" alt="...">
+  </a>
+  <div class="media-body">
+    <h4 class="media-heading">Media heading</h4>
+    ...
+  </div>
+  <div class="media">
+	  <a class="pull-left" href="#">
+	    <img class="media-object" src="web/img/vert-64x64.gif" alt="...">
+	  </a>
+	  <div class="media-body">
+	    <h4 class="media-heading">Media heading</h4>
+	    ...
+	  </div>
+	</div>
+	<div class="media">
+  <a class="pull-left" href="#">
+    <img class="media-object" src="web/img/vert-64x64.gif" alt="...">
+  </a>
+  <div class="media-body">
+    <h4 class="media-heading">Media heading</h4>
+    ...
+  </div>
+  <div class="media">
+	  <a class="pull-left" href="#">
+	    <img class="media-object" src="web/img/vert-64x64.gif" alt="...">
+	  </a>
+	  <div class="media-body">
+	    <h4 class="media-heading">Media heading</h4>
+	    ...
+	  </div>
+	</div>
+	<div class="media">
+  <a class="pull-left" href="#">
+    <img class="media-object" src="web/img/vert-64x64.gif" alt="...">
+  </a>
+  <div class="media-body">
+    <h4 class="media-heading">Media heading</h4>
+    ...
+  </div>
+  <div class="media">
+	  <a class="pull-left" href="#">
+	    <img class="media-object" src="web/img/vert-64x64.gif" alt="...">
+	  </a>
+	  <div class="media-body">
+	    <h4 class="media-heading">Media heading</h4>
+	    ...
+	  </div>
+	</div>
+	<div class="media">
+  <a class="pull-left" href="#">
+    <img class="media-object" src="web/img/vert-64x64.gif" alt="...">
+  </a>
+  <div class="media-body">
+    <h4 class="media-heading">Media heading</h4>
+    ...
+  </div>
 <?php
 $contents = ob_get_clean();
 if (is_file('template/layout/layout.php')){
