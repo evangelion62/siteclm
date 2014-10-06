9b534686c233859138450068fef11cd741fcdd53
docdoxey <vbvincentboulanger@gmail.com>
2014-10-06 14:51:04 +0200

    template : deleteuser=>création
    ajout d'une balise <p>avec un texte de mise en garde + ajout d'un bouton
    OUI et NON

--- template/user/deleteuser.php
+++ template/user/deleteuser.php
@@ -1,1 +1,18 @@
 <?php
+
+/*Accessible à ?controler=user&action=deleteuser*/
+
+ob_start();?>
+<div>
+	<span class="label label-warning">Attention</span><p> Cette action est irréversible. Etes-vous sûr de vouloir supprimer cet élément ?</p>
+</div>
+<ul class="nav nav-pills">
+  <li class="active"><a href="#">OUI</a></li>
+  <li><a href="#">NON</a></li>
+</ul>
+<?php
+$contents = ob_get_clean();
+if (is_file('template/layout/layout.php')){
+	require_once 'template/layout/layout.php';
+}
+?>
