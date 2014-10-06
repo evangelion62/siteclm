b27297e385dffa47053ada45eaf0a1bdb47b20d5
docdoxey <vbvincentboulanger@gmail.com>
2014-10-06 16:55:48 +0200

    template : modif espace deconnexion
    se deconnecter en h1 + ajout de "se reconnecter" + ajout de "retourner à
    l'accueil"

--- template/user/disconnectuser.php
+++ template/user/disconnectuser.php
@@ -1,17 +1,24 @@
 <?php
 
-/*Accessible à ?controler=user&action=deleteuser*/
+/*Accessible à ?controler=user&action=disconnectuser*/
 
 ob_start();?>
-<div>
-	<span class="label label-success">Déconnexion réussie ! </span><p> Vous êtes bien déconnectés !</p>
+<div class="jumbotron">
+  <h1><span class="label label-success">Déconnexion réussie ! </span></h1><>
+  <p><a class="btn btn-primary btn-lg" role="button">Se reconnecter ?</a></p>
+  <p><a class="btn btn-primary btn-lg" role="button">Retourner à l'accueil</a></p>
 </div>
-<ul class="nav nav-pills">
-  <li class="active"><a href="#">Se reconnecter ?</a></li>
-</ul>
+<div>
+	<div class="row">
+  	  <div class="col-md-6"><span class="glyphicon glyphicon-star"></span>.col-md-6</div>
+  	  <div class="col-md-6">.col-md-6</div>
+	</div>
+</div>
 <?php
 $contents = ob_get_clean();
 if (is_file('template/layout/layout.php')){
 	require_once 'template/layout/layout.php';
 }
 ?>
+glyphicon glyphicon-remove-circle
+
