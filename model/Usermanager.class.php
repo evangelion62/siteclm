d77ca11efa2014021c83588ee2a70c36497777aa
sebastien <sebastien.ronger.evangelion@gmail.com>
2014-10-07 14:31:47 +0200

    model : UserManager -> créa

--- model/Usermanager.class.php
+++ model/Usermanager.class.php
@@ -3,6 +3,17 @@ class UserManager {
 	private $db;
 	
 	/*constructeur*/
+	public function __construct($db)
+	{
+		$this->setDb($db);
+	}
 	
+	public function setDb(PDO $db)
+	{
+		$this->_db = $db;
+	}
 	/*CRUD*/
+	public function add($param) {
+		;
+	}
 }
