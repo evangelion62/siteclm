4d45d8c1404f8b71e3061920f66af8fcc6be9418
sebastien <sebastien.ronger.evangelion@gmail.com>
2014-10-07 15:46:57 +0200

    model : UserManager -> finalisation

--- model/Usermanager.class.php
+++ model/Usermanager.class.php
@@ -1,6 +1,6 @@
 <?php
 class UserManager {
-	private $db;
+	private $_db;
 	
 	/*constructeur*/
 	public function __construct($db)
@@ -12,8 +12,63 @@ class UserManager {
 	{
 		$this->_db = $db;
 	}
-	/*CRUD*/
-	public function add($param) {
-		;
+	public function createTable() {
+		$q= $this->_db->prepare(
+			"CREATE TABLE IF NOT EXISTS `user` (
+			id int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'idex primaire',
+			`name` varchar(255) NOT NULL COMMENT 'nom d''utilisateur',
+			pass varchar(255) NOT NULL COMMENT 'mot de passe',
+			mail varchar(255) DEFAULT NULL COMMENT 'e mail',
+			PRIMARY KEY (id)
+			) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1"
+		);
+		$q->execute();
 	}
+	/*CRUD*/
+	public function add(User $user) {
+		$q = $this->_db->prepare("INSERT INTO user SET name = :name, pass = :pass, mail = :mail");
+		
+		$q->bindValue(':name', $user->name());
+		$q->bindValue(':pass', $user->pass());
+		$q->bindValue(':mail', $user->mail());
+		
+		$q->execute();
+	}
+	
+	public function get($id) {
+		$id = (int) $id;
+		
+		$q = $this->_db->query("SELECT * FROM user WHERE id =".$id);
+		$donnees = $q->fetch(PDO::FETCH_ASSOC);
+		
+		return new User($donnees);
+	}
+	
+	public function getList()
+	{
+		$users = array();
+		
+		$q = $this->_db->query('SELECT * FROM user ORDER BY name');
+		while ($donnees = $q->fetch(PDO::FETCH_ASSOC))
+		{
+			$users[] = new User($donnees);
+		}
+		return $users;
+	}
+	
+	public function update(User $user) {
+		$q = $this->_db->prepare("UPDATE user SET name = :name, pass = :pass, mail = :mail WHERE id = :id");
+		
+		$q->bindValue(':name', $user->name());
+		$q->bindValue(':pass', $user->pass());
+		$q->bindValue(':mail', $user->mail());
+		$q->bindValue(':id', $user->id());
+		
+		$q->execute();
+	}
+	
+	public function delete($id) {
+		$this->_db->exec('DELETE FROM user WHERE id ='.$id);
+	}
+	
 }
