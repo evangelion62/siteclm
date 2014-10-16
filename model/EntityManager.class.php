f16482ffc61cf671909f366e8ec4e3da730a1f0f
sebastien <sebastien.ronger.evangelion@gmail.com>
2014-10-16 15:59:13 +0200

    model : orm -> modif

--- model/EntityManager.class.php
+++ model/EntityManager.class.php
@@ -1,11 +1,13 @@
 <?php
 abstract class EntityManager {
 	protected $_db;
+	protected $_entityName;
 	
 	/*constructeur*/
 	public function __construct($db)
 	{
 		$this->setDb($db);
+		$this->_entityName = str_replace('Manager', '', get_class($this));
 	}
 	
 	public function setDb(PDO $db)
@@ -15,7 +17,7 @@ abstract class EntityManager {
 	
 	/*création des tables de l'entité*/
 	public function createTable() {
-		$entityName = str_replace('Manager', '', get_class($this));
+		$entityName = $this->_entityName;
 		$entityProperties = $entityName::$_properties;
 		foreach ($entityProperties as $table => $properties) {
 			
@@ -36,9 +38,113 @@ abstract class EntityManager {
 				PRIMARY KEY (id)
 				) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1"
 			);
-			$q->execute();
-			echo $q->queryString;
+			$result = $q->execute();
 			$q->closeCursor();
+			return $result;
 		}
+	}
+	
+	/*CRUD*/
+	
+	/*CREAT A NEW ENTRY*/
+	public function add($entity) {
+		if ($this->_entityName == get_class($entity)){
+			$entityName = $this->_entityName;
+			$entityProperties = $entityName::$_properties;
+			foreach ($entityProperties as $table => $properties) {
+					
+				$propertiesStr = '';
+				foreach ($properties as $keyName => $dbParams) {
+					$propertiesStr = $propertiesStr.' '.$keyName.' = :'.$keyName.',';
+				}
+				$propertiesStr = substr($propertiesStr,0,strlen($propertiesStr)-1);
+				
+				$q = $this->_db->prepare("INSERT INTO `".$table."` SET".$propertiesStr);
+				
+				foreach ($properties as $keyName => $dbParams) {
+					$q->bindValue(':'.$keyName, $entity->$keyName());
+				}
+				
+				$result = $q->execute();
+				$q->closeCursor();
+				return $result;
+			}
+		}
+		else {
+			throw new Exception('l\'entity et l\'entityManager sont incompatible.');
+		}
+	}
+	
+	/*renvoi l'entité demandé*/
+	public function get($value,$selectKeyName = 'id') {
+		$entityName = $this->_entityName;
+		$entityProperties = $entityName::$_properties;
+		$donnees = array();
+		foreach ($entityProperties as $table => $properties) {
+			if (is_string($value)){
+				$q = $this->_db->query("SELECT * FROM `".$table."` WHERE `".$selectKeyName."` = '".$value."'");
+			}else {
+				$q = $this->_db->query("SELECT * FROM `".$table."` WHERE `".$selectKeyName."` = ".$value);
+			}
+			
+			$donnees = $q->fetch(PDO::FETCH_ASSOC);
+			return new $entityName($donnees);
+		}
+	}
+	
+	/*revoi la liste de toutes les entité demandé*/
+	public function getList($orderBy='id')
+	{
+		$entityName = $this->_entityName;
+		$entityProperties = $entityName::$_properties;
+		$entitys = array();
+		
+		foreach ($entityProperties as $table => $properties) {
+			$q = $this->_db->query("SELECT * FROM `".$table."` ORDER BY ".$orderBy);
+			while ($donnees = $q->fetch(PDO::FETCH_ASSOC))
+			{
+				$entitys[] = new $entityName($donnees);
+			}
+			return $entitys;
+		}
+	}
+	
+	public function update($entity) {
+		
+		if ($this->_entityName == get_class($entity)){
+			$entityName = $this->_entityName;
+			$entityProperties = $entityName::$_properties;
+			foreach ($entityProperties as $table => $properties) {
+					
+				$propertiesStr = '';
+				foreach ($properties as $keyName => $dbParams) {
+					$propertiesStr = $propertiesStr.' '.$keyName.' = :'.$keyName.',';
+				}
+				$propertiesStr = substr($propertiesStr,0,strlen($propertiesStr)-1);
+		
+				$q = $this->_db->prepare("UPDATE `".$table."` SET".$propertiesStr." WHERE id = ".$entity->id());
+		
+				foreach ($properties as $keyName => $dbParams) {
+					$q->bindValue(':'.$keyName, $entity->$keyName());
+				}
+		
+				$result = $q->execute();
+				print_r($q->queryString);
+				$q->closeCursor();
+				return $result;
+			}
+		}
+		else {
+			throw new Exception('l\'entity et l\'entityManager sont incompatible.');
+		}
+		
+// 		$q = $this->_db->prepare("UPDATE user SET name = :name, pass = :pass, mail = :mail WHERE id = :id");
+	
+// 		$q->bindValue(':name', $user->name());
+// 		$q->bindValue(':pass', $user->pass());
+// 		$q->bindValue(':mail', $user->mail());
+// 		$q->bindValue(':id', $user->id());
+	
+// 		$q->execute();
 	}
 }
--- model/Usermanager.class.php
+++ model/Usermanager.class.php
@@ -2,48 +2,38 @@
 class UserManager extends EntityManager{
 	
 	
-	/*CRUD*/
-	public function add(User $user) {
-		$q = $this->_db->prepare("INSERT INTO user SET name = :name, pass = :pass, mail = :mail");
+	/*CRUD*/	
+// 	public function get($id) {
+// 		$id = (int) $id;
 		
-		$q->bindValue(':name', $user->name());
-		$q->bindValue(':pass', $user->pass());
-		$q->bindValue(':mail', $user->mail());
+// 		$q = $this->_db->query("SELECT * FROM user WHERE id =".$id);
+// 		$donnees = $q->fetch(PDO::FETCH_ASSOC);
 		
-		$q->execute();
-	}
+// 		return new User($donnees);
+// 	}
 	
-	public function get($id) {
-		$id = (int) $id;
+// 	public function getList()
+// 	{
+// 		$users = array();
 		
-		$q = $this->_db->query("SELECT * FROM user WHERE id =".$id);
-		$donnees = $q->fetch(PDO::FETCH_ASSOC);
-		
-		return new User($donnees);
-	}
+// 		$q = $this->_db->query('SELECT * FROM user ORDER BY name');
+// 		while ($donnees = $q->fetch(PDO::FETCH_ASSOC))
+// 		{
+// 			$users[] = new User($donnees);
+// 		}
+// 		return $users;
+// 	}
 	
-	public function getList()
-	{
-		$users = array();
+// 	public function update(User $user) {
+// 		$q = $this->_db->prepare("UPDATE user SET name = :name, pass = :pass, mail = :mail WHERE id = :id");
 		
-		$q = $this->_db->query('SELECT * FROM user ORDER BY name');
-		while ($donnees = $q->fetch(PDO::FETCH_ASSOC))
-		{
-			$users[] = new User($donnees);
-		}
-		return $users;
-	}
-	
-	public function update(User $user) {
-		$q = $this->_db->prepare("UPDATE user SET name = :name, pass = :pass, mail = :mail WHERE id = :id");
+// 		$q->bindValue(':name', $user->name());
+// 		$q->bindValue(':pass', $user->pass());
+// 		$q->bindValue(':mail', $user->mail());
+// 		$q->bindValue(':id', $user->id());
 		
-		$q->bindValue(':name', $user->name());
-		$q->bindValue(':pass', $user->pass());
-		$q->bindValue(':mail', $user->mail());
-		$q->bindValue(':id', $user->id());
-		
-		$q->execute();
-	}
+// 		$q->execute();
+// 	}
 	
 	public function delete($id) {
 		$this->_db->exec('DELETE FROM user WHERE id ='.$id);
