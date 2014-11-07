1a0bfe0dad37c00a4247f6ef8e8d3930df393733
docdoxey <vbvincentboulanger@gmail.com>
2014-11-07 11:16:12 +0100

    template : getactu => creation

---
+++ template/actu/getactu.php
@@ -0,0 +1,11 @@
+<?php ob_start();?>
+<h1><?php echo $actu->name() ?></h1>
+<p><?php echo $actu->content()?></p>
+	<?php ?>
+<?php
+$contents = ob_get_clean ();
+$ckeditor = true;
+if (is_file ( 'template/layout/layout.php' )) {
+	require_once 'template/layout/layout.php';
+}
+?>
