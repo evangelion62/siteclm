a9e997269f27215dd2dfcc73331b2ad87c8a2e24
sebastien <sebastien.ronger.evangelion@gmail.com>
2015-01-13 15:18:21 +0100

    module -> connection utilisateur
    procédure de connection

--- controlers/user.php
+++ controlers/user.php
@@ -30,11 +30,49 @@ switch ($action){
 	break;
 		
 	case 'connectUser':
-		require_once 'template/user/connectuser.php';
+		if (	!empty($_POST['userId']	)
+			&&	!empty($_POST['userMdp']	)){
+			$userManager = new UserManager($bdd);
+			$user = $userManager->get($_POST['userId'],'name');
+			if ($user){
+				if ($_POST['userMdp']==$user->pass()){
+					//auth ok => generation d'un token
+					$token = md5($user->pass().$_SERVER["REMOTE_ADDR"].'abc123');
+					$donnees= array('userid'=>$user->id(),'token'=>$token);
+					$userAuth = new UserAuth($donnees);
+					$userAuthManager = new UserAuthManager($bdd);
+					if ($entity = $userAuthManager->get($user->id(),'userid')){
+						$userAuthManager->update($entity);
+						$_SESSION['userid']=$user->id();
+						$_SESSION['token']=$token;
+						header ( 'Location: ?controler=index' );
+					}else{
+						$userAuthManager->add($userAuth);
+						$_SESSION['userid']=$user->id();
+						$_SESSION['token']=$token;
+						header ( 'Location: ?controler=index' );
+					}
+					
+				}else{
+					$userErrors['userPassError']="mot de passe utilisateur incorect";
+					$formAction = '?controler=user&action=connectUser';
+					require_once 'template/user/connectuser.php';
+				}
+			}else{
+			//le nom d'utilisateur n'existe pas dans la base
+				$userErrors['userNameError']="nom d'utilisateur incorect";
+				$formAction = '?controler=user&action=connectUser';
+				require_once 'template/user/connectuser.php';
+			}
+		}else{
+			$formAction = '?controler=user&action=connectUser';
+			require_once 'template/user/connectuser.php';
+		}
 	break;
 	
 	case 'disconnectUser':
-		require_once 'template/user/disconnectuser.php';
+		$_SESSION = array();
+		header ( 'Location: ?controler=index' );
 	break;
 	
 	default:
