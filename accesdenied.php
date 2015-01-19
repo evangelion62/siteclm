2bda0e6bc6bcc0f60dce5bd1a99f478e1a7a5bb5
VINCENT BOULANGER <Vincent@MBP-de-VINCENT.louisemichel.com>
2015-01-19 11:38:29 +0100

    correction texte accès denied
    correction sémantique du texte

---
+++ accesdenied.php
@@ -0,0 +1,14 @@
+<?php
+
+/*Accessible à ?controler=user&action=disconnectuser*/
+
+ob_start();?>
+<h1>Accès refusé</h1>
+<span class="glyphicon glyphicon-lock" aria-hidden="true"></span>
+<p>Vous n'avez pas accès à cette page, vous ne disposez des droits suffisants. Veuillez contacter le webmaster pour toutes informations supplémentaires</p>
+<?php
+$contents = ob_get_clean();
+if (is_file('template/layout/layout.php')){
+	require_once 'template/layout/layout.php';
+}
+?>
